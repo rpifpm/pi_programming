81cea40462326f80dbbd50ecd77f0a8c9f81321d
rpifpm <rpifpm@gmail.com>
2019-05-26 19:43:22 +0000

    main

--- watcher.py
+++ watcher.py
@@ -6,52 +6,47 @@ import configparser
 #functions
 
 def configure():
+	conf={}
 	#read the ini file
 	inifile = '../config/watcher.ini'
 	config = configparser.ConfigParser()
 	config.read(inifile)
   
 	# define attach file
-	atfile = '/home/pi/foto2.jpg'  
+	conf['atfile'] = config['mail']['atfile']
  
   
 	# Define email addresses to use
-	addr_to   = config['mail']['addr_to']
-	addr_from = config['mail']['addr_from']
+	conf['addr_to']   = config['mail']['addr_to']
+	conf['addr_from'] = config['mail']['addr_from']
   
 	# Define SMTP email server details
-	smtp_server = config['mail']['smtp_server']
-	smtp_user = config['mail']['smtp_user']
-	smtp_pass = config['mail']['smtp_pass']
+	conf['smtp_server'] = config['mail']['smtp_server']
+	conf['smtp_user'] = config['mail']['smtp_user']
+	conf['smtp_pass'] = config['mail']['smtp_pass']
+	
+	return conf
+	
+def takePhoto(conf):
+	atfile = conf.get('atfile')
+	stringPhoto = 'fswebcam -r 640x480' + atfile
+	os.system(stringPhoto) 
 	
 	return
 
-# Import the email modules
-from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.mime.multipart import MIMEMultipart
-from email import encoders
-from email.mime.base import MIMEBase
-
-configure()
-
-while True:
-	#takes photo
-	os.system('fswebcam -r 640x480 /home/pi/foto2.jpg') 
-	  
-	# Construct email
+def sendMail(conf):	
 	msg = MIMEMultipart()
-	msg['To'] = addr_to
-	msg['From'] = addr_from
-	msg['Subject'] = 'Photo Test Email From RPi'
+	msg['To'] = conf.get('addr_to')
+	msg['From'] = conf.get('addr_from')
+	msg['Subject'] = 'Photo Email From RPi'
 
-	body ='Prueba de envio de fotografia'
+	body ='Photo from home'
 
 	# Add body to email
 	msg.attach(MIMEText(body, "plain"))
 
-	filename = atfile  
-
+	filename = conf.get('atfile')
+	
 	# Open file in binary mode
 	with open(filename, "rb") as attachment:
 		# Add file as application/octet-stream
@@ -74,16 +69,36 @@ while True:
 	  
 	# Send the message via an SMTP server
 	try:
-	  s = smtplib.SMTP(smtp_server)
+	  s = smtplib.SMTP(conf.get('smtp_server'))
 	  s.ehlo()
 	  s.starttls()
-	  s.login(smtp_user,smtp_pass)
-	  s.sendmail(addr_from, addr_to, text)
+	  s.login(conf.get('smtp_user'),conf.get('smtp_pass'))
+	  s.sendmail(conf.get('addr_from'), conf.get('addr_to'), text)
 	  s.quit()
 	except Exception, ex1:
 	  print("There was an error sending the email. Check the smtp settings.")
 	  print ex1
+	  
+	return
 
+# Import the email modules
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+from email.mime.base import MIMEBase
+
+conf = configure()
+
+#main loop 
+while True:
+	#takes photo
+	takePhoto(conf)
+	  
+	# send email
+	sendMail(conf)
+
+	# wait
 	time.sleep(1*60*60)
 
 
